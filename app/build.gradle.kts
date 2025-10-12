4e0c558267354657457ab5b56274ecec9b379e7d
Enos Andrade <enosads@gmail.com>
2025-10-12 02:17:09 -0300

    Chore: Update libraries and dependencies, refactor dependencies block
    - Updated library versions in `libs.versions.toml`:
      - Kotlin to `2.2.20`
      - Core KTX to `1.17.0`
      - Lifecycle Runtime KTX, Compose BOM, AppCompat, Material, and more.
      - Added Navigation dependencies (Fragment KTX, UI KTX).
    - Introduced `androidx` bundle for grouped dependencies.
    - Refactored `app/build.gradle.kts` to use the `androidx` bundle for cleaner dependency management.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,7 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.bundles.androidx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -49,10 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
